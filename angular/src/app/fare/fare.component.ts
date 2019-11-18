e92f0f94dd9b5971f7540afb5f45ae0d22a5fb67
rahul.ranjan04 <rahul.ranjan04@gmail.com>
2019-11-18 12:23:39 +0530

    removing deleted files from tracking

---
+++ angular/src/app/fare/fare.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import 'rxjs/Rx';
+import { AppService } from '../app.service';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './fare.component.html',
+  styleUrls: ['../app.component.css', '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'],
+  providers: [AppService]
+})
+
+export class FareComponent implements OnInit {
+
+
+  fare: any;
+  term: any;
+  showSelected: boolean;
+
+  originpath: string;
+  destination: string;
+  listdata: string;
+
+  queryField: FormControl = new FormControl();
+  queryField1: FormControl = new FormControl();
+
+
+  constructor(private appService: AppService) {
+    this.appService = appService;
+  }
+
+  ngOnInit() {
+
+    this.showSelected = false;
+
+    this.queryField.valueChanges.debounceTime(1000).subscribe(() => {
+      if (this.originpath !== undefined) {
+        this.appService.doGetSearch(this.originpath).subscribe(data => this.term = data);
+      }
+    });
+
+    this.queryField1.valueChanges.debounceTime(1000).subscribe(() => {
+      if (this.destination !== undefined) {
+        this.appService.doGetSearch(this.destination).subscribe(data => this.term = data);
+      }
+    });
+
+
+
+
+  }
+  doGetFares() {
+    this.appService.doGetFare(this.originpath, this.destination).subscribe(data => this.fare = data);
+  }
+
+
+
+
+}
---
+++ angular/src/app/stats/stats.component.ts
@@ -0,0 +1,31 @@
+import { Component,OnInit } from '@angular/core';
+import { debounceTime, map } from 'rxjs/operators';
+import {AppService} from '../app.service';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './stats.component.html',
+  styleUrls: ['../app.component.css', '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'],
+  providers : [AppService]
+})
+
+export class StatsComponent implements OnInit {
+
+statistics:any;
+
+constructor(private appService: AppService) {
+  this.appService=appService;
+ }
+
+ ngOnInit() {
+this.appService.doGetStatistics().subscribe(data => this.statistics = data);
+}
+doGetStatistics(){
+this.appService.doGetStatistics().subscribe(data => this.statistics = data);
+}
+
+
+
+
+}
